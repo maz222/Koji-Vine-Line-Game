398f17bef0663a7e933b1e5b728702af16cec1e0
Svarog1389 <Svarog1389@koji-projects.com>
2019-08-26 12:56:48 +0000

    Manual deploy

--- frontend/app/easing.js
+++ frontend/app/easing.js
@@ -34,7 +34,40 @@ EasingFunctions = {
     easeOutElastic: function (t) {
         var p = 0.3;
         return Math.pow(2, -10 * t) * Math.sin((t - p / 4) * (2 * Math.PI) / p) + 1;
+    },
+
+    inBack: function (t) {
+        var s = 1.70158;
+        return t * t * ((s + 1) * t - s);
+    },
+
+    outBack: function (t) {
+        var s = 1.70158;
+        return --t * t * ((s + 1) * t + s) + 1;
+    },
+
+    outBounce: function (t) {
+        if (t < (1 / 2.75)) {
+            return 7.5625 * t * t;
+        } else if (t < (2 / 2.75)) {
+            return 7.5625 * (t -= (1.5 / 2.75)) * t + 0.75;
+        } else if (t < (2.5 / 2.75)) {
+            return 7.5625 * (t -= (2.25 / 2.75)) * t + 0.9375;
+        } else {
+            return 7.5625 * (t -= (2.625 / 2.75)) * t + 0.984375;
+        }
+    },
+
+    inOutElastic: function (t) {
+        var s, a = 0.1, p = 0.4;
+        if (t === 0) return 0;
+        if (t === 1) return 1;
+        if (!a || a < 1) { a = 1; s = p / 4; }
+        else s = p * Math.asin(1 / a) / (2 * Math.PI);
+        if ((t *= 2) < 1) return - 0.5 * (a * Math.pow(2, 10 * (t -= 1)) * Math.sin((t - s) * (2 * Math.PI) / p));
+        return a * Math.pow(2, -10 * (t -= 1)) * Math.sin((t - s) * (2 * Math.PI) / p) * 0.5 + 1;
     }
+
 
 }
 
@@ -45,10 +78,22 @@ Example usage:
 this.pos.x = Ease(EasingFunctions.easeOutQuad, this.animTimer, this.pos.x, this.goalPos.x - this.pos.x, 1);
 this.pos.y = Ease(EasingFunctions.easeOutQuad, this.animTimer, this.pos.y, this.goalPos.y - this.pos.y, 1);
 */
+/*
 function Ease(func, time, start, finish, duration) {
-    if ((time /= duration / 2) < 1) {
+    if ((time / duration) < 1) {
         return finish / 2 * func(time) + start;
     } else {
         return -finish / 2 * ((--time) * (time - 2) - 1) + start;
     }
+}
+
+*/
+
+function Ease(func, time, start, finish, duration) {
+    let t = time;
+    if (time > 1) {
+        t = 1;
+    }
+    return finish * func(t) + start;
+
 }
